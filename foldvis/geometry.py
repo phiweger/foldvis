2742a84ba51c6136b4366073b86747facd855b05
phiweger <3918306+phiweger@users.noreply.github.com>
2022-07-14 10:59:19 +0200

    Getis-Ord statistic implemented

--- foldvis/geometry.py
+++ foldvis/geometry.py
@@ -1,23 +1,72 @@
 # import shutil
 import subprocess
 import tempfile
+from typing import Union
 
+from Bio.PDB.Residue import Residue
+from Bio.PDB.Atom import Atom
 import numpy as np
 import screed
 
 
-def is_close(pos, fold, radius):
+def get_alpha_carbon_atoms(fold, only_coords=False):
+    '''
+    alpha carbon: https://foldit.fandom.com/wiki/Alpha_carbon
+    '''
+    for res in fold.structure.get_residues():
+        l = []
+        for i in res.get_atoms():
+            # There is only one alpha carbon per amino acid.
+            if i.get_id() == 'CA':
+                if only_coords:
+                    yield i.coord
+                else:
+                    yield i
+
+
+def get_coordinate(x: Union[Atom, Residue]):
+    '''
+    Calculating center of mass is much slower than looking up the coordinate
+    of a carbon atom.
+    '''
+    if type(x) == Residue:
+        return x.center_of_mass()
+    elif type(x) == Atom:
+        return x.coord
+    else:
+        raise ValueError('Unsupported type')
+
+
+def is_close(pos, fold, radius, coordinates='alpha_carbons'):
     '''
     fold = Fold('test_676a7_unrelaxed_rank_1_model_2.pdb')
     list(is_close(1, fold, 10))
     # [True, True, True, True, True, False, False, ...]
-    '''
-    residues = list(fold.structure.get_residues())
-    ref = residues[pos]
-    a = ref.center_of_mass()
 
-    for res in residues:
-        b = res.center_of_mass()
+    coordinates .. center_of_mass, alpha_carbon
+
+    TODO: pseudo single-atom representation of side chains:
+
+    > Specifically, we defined this distance according to the sites' side chain
+    center of masses. A consequence of approximating DTL with respect to the
+    closest ligand-binding sites is that by definition, any ligand-binding
+    residue has a DTL of 0. -- Kiefl et al.,
+    https://www.biorxiv.org/content/10.1101/2022.03.02.482602v1.full.pdf
+
+    - https://pymolwiki.org/index.php/Sidechaincenters
+    - https://bioinformatics.stackexchange.com/questions/18162/pymol-python-script-for-selecting-a-residues-sidechain-and-calculating-its-cent
+    '''
+
+    if coordinates == 'center_of_mass':
+        chain = list(fold.structure.get_residues())
+    
+    elif coordinates == 'alpha_carbons':
+        chain = list(get_alpha_carbon_atoms(fold))
+    
+    a = get_coordinate(chain[pos])
+
+    for i in chain:
+        b = get_coordinate(i)
         # https://stackoverflow.com/questions/1401712/how-can-the-euclidean-distance-be-calculated-with-numpy
         dist = np.linalg.norm(a - b)
         if dist < radius:
---
+++ foldvis/stats.py
@@ -0,0 +1,105 @@
+import numpy as np
+
+from foldvis.geometry import is_close
+
+
+def spatial_association(fold, features, statistic='Gi', radius=8, coordinates='alpha_carbons'):
+    '''
+    Calculates statistic(s) that can be used to test for spatial
+    association.
+
+    TODO:
+
+    - multiple comparison correction (control FDR)
+    '''
+    assert len(fold) == len(features), 'Residue to feature mapping not unique'
+
+    l = []
+    for pos in range(len(fold)):
+        sphere = list(is_close(pos, fold, radius, coordinates))
+    
+        fn = globals()[statistic]
+        # https://stackoverflow.com/questions/3061/calling-a-function-of-a-module-by-using-its-name-a-string
+
+        _, z = fn(sphere, features, pos)
+        l.append(z)
+    
+    return l
+
+
+def Gi(w_ij, features, pos):
+    '''
+    Getis-Ord statistic for spatial association.
+
+    "The analysis of Spatial Association by Use of Distance Statistics",
+    Getis & Ord, Geographical Analysis, 1992
+
+    Gi, other than Gi_star (see below) will exclude the position in the
+    "center" of the current fn call (i != j).
+    '''
+    n = len(w_ij)
+
+    # exclude the query position
+    w_ij = w_ij[:pos] + [False] + w_ij[pos+1:]
+    features = features[:pos] + [0] + features[pos+1:]
+
+    scores = sum([score for include, score in zip(w_ij, features) if include])
+    total = sum(features)
+    G = scores / total
+    
+    Wi = sum(w_ij)
+    Yi1 = total / (n-1)
+    
+    x = sum(i**2 for i in features)
+    Yi2 = (x / (n-1)) - (Yi1**2)
+    
+    E = Wi / (n-1)
+    Var = (Wi * (n-1-Wi) * Yi2) / ((n-1)**2 * (n-2) * Yi1**2)
+    Z = (G-E) / np.sqrt(Var)
+    
+    return G, Z
+
+
+def Gi_star(w_ij, features, pos):
+    '''
+    "The analysis of Spatial Association by Use of Distance Statistics",
+    Getis & Ord, Geographical Analysis, 1992
+
+    Gi_star, other than Gi (see above) will include the position in the
+    "center" of the current fn call (i ==j).
+    '''
+    n = len(w_ij)
+
+    scores = sum([score for include, score in zip(w_ij, features) if include])
+    total = sum(features)
+    G = scores / total
+
+    Wi = sum(w_ij)
+    Yi1 = total / n
+
+    x = sum((features[pos] * i)**2 for i in features)
+    Yi2 = (x / n) - (Yi1**2)
+    # The double sum in table 1 of the original paper just means add x_i to
+    # the sum of all the other features.
+
+    E = Wi / n
+    Var = (Wi * (n-Wi) * Yi2) / (n**2 * (n-1) * Yi1**2)
+    Z = (G-E) / np.sqrt(Var)
+    
+    return G, Z
+
+
+'''
+TODO:
+
+random forest, predict clusters from features, then variable importance to see
+which features matter most.
+
+==?
+
+enrichment: given spatial clusters, are they enriched in any feature?
+
+- distance to ligand
+- solvent accessibility
+- interface
+'''
